6b509b99dbb05c842e6bc7ac68e681b2da6a6a57
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-01-21 23:59:47 +0100

    login page video

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import shareVideo from '../assets/share.mp4'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='flex justify-start items-center flex-col h-screen'>
+        <div className='relative w-full h-full'>
+            <video
+                src={shareVideo}
+                type='video/mp4'
+                loop
+                controls={false}
+                muted
+                autoPlay
+                className='w-full h-full object-cover'
+            />
+        </div>
+    </div>
   )
 }
 
