fadc44e7b52fb4e1c6ccf0d85e7cff1505d0e6c8
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-01-21 22:19:52 +0100

    schemas added

---
+++ backend/schemas/user.ts
@@ -0,0 +1,22 @@
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    feilds: [
+        {
+            name: 'userName',
+            title: 'UserName',
+            type: 'string'
+        },
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'string'
+        },
+        {
+            name: 'userID',
+            title: 'UserID',
+            type: 'string'
+        }
+    ]
+}
