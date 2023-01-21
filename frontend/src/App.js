00fccf9d66a7f0bffd9ae61ac9ba03109e02c058
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-01-21 23:44:06 +0100

    Basic App components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+import Login from './components/Login'
+import Home from './container/Home'
 
 function App() {
   return (
-    <div>App</div>
+    <Routes>
+        <Route path='login' element={<Login />} />
+        <Route path='home' element={<Home />} />
+    </Routes>
   )
 }
 
