00788b5c8ab7d9bf1e7e1151e75803731a63248a
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-01-29 17:03:39 +0100

    save and download pin

--- frontend/src/container/Home.jsx
+++ frontend/src/container/Home.jsx
@@ -9,6 +9,7 @@ import Pins from './Pins'
 import { userQuery } from '../utils/data'
 import { client } from '../client'
 import logo from '../assets/logo.png'
+import { fetchUser } from '../utils/fetchUser'
 
 
 const Home = () => {
@@ -16,7 +17,7 @@ const Home = () => {
   const [user, setUser] = useState(null)
   const scrollRef = useRef(null)
 
-  const userInfo = localStorage.getItem('user') !== 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear()
+  const userInfo = fetchUser
 
   useEffect(() => {
     const query = userQuery(userInfo?.sub);
