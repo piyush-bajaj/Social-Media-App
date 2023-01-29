a5610b59fc38082b29597ee699948bc844b253b1
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-01-29 13:35:07 +0100

    sidebar with demo data

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -7,6 +7,15 @@ import logo from '../assets/logo.png'
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:black transition-all duration-200 ease-in-out capitalize'
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize'
 
+const categories = [
+  {name: 'Animals'},
+  {name: 'Wallpapers'},
+  {name: 'Photography'},
+  {name: 'Gaming'},
+  {name: 'Coding'},
+  {name: 'Other'},
+]
+
 const Sidebar = ({user, closeToggle}) => {
 
   const handleCloseSidebar = () => {
@@ -27,14 +36,36 @@ const Sidebar = ({user, closeToggle}) => {
           <NavLink
             to='/'
             className={({isActive}) => isActive ? isActiveStyle : isNotActiveStyle}
+            onClick={handleCloseSidebar}
             >
               <RiHomeFill />
               Home
           </NavLink>
           <h3 className='mt-2 px-5 text-base 2xl:text-xl'>Discover categories</h3>
+          {
+            categories.slice(0, categories.length - 1).map((category) =>
+              <NavLink
+                to={`/category/${category.name}`}
+                className={({isActive}) => isActive ? isActiveStyle : isNotActiveStyle}
+                onClick={handleCloseSidebar}
+                key={category.name}
+              >
+                {category.name}
+              </NavLink>
+            )
+          }
         </div>
       </div>
-      Sidebar
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className='flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3'
+          onClick={handleCloseSidebar}
+        >
+          <img src={user.image} className='w-10 h-10 rounded-full' alt='user.profile'/>
+          <p>{user.userName}</p>
+        </Link>
+      )}
     </div>
   )
 }
