7df4aa24a61fcd691cf558ac09817b6622a49056
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-01-29 15:42:14 +0100

    navbar component

--- frontend/src/container/Pins.jsx
+++ frontend/src/container/Pins.jsx
@@ -9,7 +9,7 @@ const Pins = ({user}) => {
   return (
     <div className='px-2 md:px-5'>
       <div className='bg-gray-50'>
-        <Navbar searchTerm={searchTerm} setsearchTerm={setsearchTerm} />
+        <Navbar searchTerm={searchTerm} setsearchTerm={setsearchTerm} user={user} />
         <div className='h-full'>
           <Routes>
             <Route path='/' element={<Feed />} />
