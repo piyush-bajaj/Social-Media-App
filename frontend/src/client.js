39cbe22fc8fd1e198dc8a71447c258d670589738
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-01-30 12:05:26 +0100

    cdn config and redirection for netlify

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -5,7 +5,7 @@ export const client = sanityClient({
     projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-01-25',
-    useCdn: false,
+    useCdn: true,
     token: process.env.REACT_APP_SANITY_TOKEN
 });
 
