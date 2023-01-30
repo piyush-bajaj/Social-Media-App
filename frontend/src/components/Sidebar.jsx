dce8e3b959f00216deecabc54169b9591d4f5376
piyush-bajaj <bajaj.piyush.1995@gmail.com>
2023-01-30 01:42:31 +0100

    category list sync

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -2,19 +2,11 @@ import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
 import { RiHomeFill } from 'react-icons/ri'
 import { IoIosArrowForward } from 'react-icons/io'
+import { categories } from '../utils/data'
 
 import logo from '../assets/logo.png'
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:black transition-all duration-200 ease-in-out capitalize'
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize'
-
-const categories = [
-  {name: 'Animals'},
-  {name: 'Wallpapers'},
-  {name: 'Photography'},
-  {name: 'Gaming'},
-  {name: 'Coding'},
-  {name: 'Other'},
-]
 
 const Sidebar = ({user, closeToggle}) => {
 
